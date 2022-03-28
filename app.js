f2268fb9cc45da25ca52195f39a4ecdcde78c2f1
Jose viejo <dviejo@kungfusoftware.es>
2022-03-28 11:59:09 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+app.get("/", function (request, response) {
+    response.send("Hi there, welcome to my assignment!");
+})
+
+app.get("/ping", (request, response) => {
+    response.send(new Date().toISOString());
+}
+)
+
+app.listen(4444, function () {
+    console.log('listening on port 4444');
+})
